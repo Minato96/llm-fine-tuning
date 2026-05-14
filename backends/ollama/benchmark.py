327f31f8f8e534aa96b62427ffa604e8a08c44c3
Minato96 <charanteja1415@gmail.com>
2026-05-14 12:29:20 +0000

    coded my own bench mark to handle the requests from ollama using requests and find the latency, throughput too

---
+++ backends/ollama/benchmark.py
@@ -0,0 +1,41 @@
+import requests
+import time
+import json
+
+def run_single(model, prompt):
+    start = time.time()
+    latency = None
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model":model,
+            "prompt":prompt,
+            "stream":True
+        },
+        stream=True
+    )
+    tokens_count = 0
+    output=""
+    for line in response.iter_lines():
+        if line:
+            line =json.loads(line)
+            if latency is None and line['response']!="":
+                latency = time.time()-start
+            if line['response']!="":
+                tokens_count += 1
+            output += line['response']
+            if(line['done']):
+                elapsed = time.time() - start
+                break
+
+    return{
+        "latency": latency,
+        "elapsed":elapsed,
+        "tokens_count": tokens_count,
+        "throughput": tokens_count/(elapsed-latency),
+        "output": output
+    }
+
+
+result = run_single("phi3-local", "What is the meaning of life?")
+print(result)
