6695aae2cb319e7fa95395d75c4e2657cb73f2dc
Minato96 <charanteja1415@gmail.com>
2026-05-18 02:17:56 +0000

    fixed load_router_exemplars arguements. Added latency calculation in route function. Fixed the json and added new and more clear prompts

--- phase2-routing/router/embedding.py
+++ phase2-routing/router/embedding.py
@@ -1,20 +1,20 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sentence_transformers import SentenceTransformer
 import json
-
+import time
 
 class EmbeddingRouter:
     def __init__(self,weak_model="qwen2.5:0.5b", strong_model="phi3-local", n_neighbors=3):
         self.weak_model = weak_model
         self.strong_model = strong_model
         self.n_neighbors = n_neighbors
-        self.knn = KNeighborsClassifier(n_neighbors=self.n_neighbors,similarity='cosine')
+        self.knn = KNeighborsClassifier(n_neighbors=self.n_neighbors,metric='cosine')
         self.embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
         self.routes = self.load_router_exemplars()
         self._train_router()
         print("EmbeddingRouter initialized with weak model:", self.weak_model, "and strong model:", self.strong_model)
 
-    def load_router_exemplars(filepath="phase2-routing/evaluation/test_prompts.json"):
+    def load_router_exemplars(self,filepath="phase2-routing/evaluation/test_prompts.json"):
         with open(filepath, "r") as file:
             data = json.load(file)
 
@@ -36,8 +36,17 @@ class EmbeddingRouter:
         self.knn.fit(X, y)
     
     def route(self, query):
+        start = time.time()
         query_embedding = self.embedding_model.encode(query).reshape(1, -1)
         predicted_model = self.knn.predict(query_embedding)[0]
-        print(f"Routing query: '{query}' to model: '{predicted_model}'")
-        return predicted_model
+        end = time.time()
+        print(f"Routing query: '{query}' to model: '{predicted_model}' (Time: {end - start:.2f}s)")
+        return {
+            "model": predicted_model,
+            "latency": end - start
+        }
 
+if __name__ == "__main__":
+    router = EmbeddingRouter()
+    test_query = "explain quantum entanglement"
+    router.route(test_query)
